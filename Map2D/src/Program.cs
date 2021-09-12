4da96d099f0be57e1f61176077fcd7e1046229c8
Leon Steinbach <leon@steinba.de>
2021-09-12 16:19:14 +0200

    Added project

---
+++ Map2D/src/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Map2D.core;
+
+namespace Map2D
+{
+	public static class Program
+	{
+		[STAThread]
+		static void Main()
+		{
+			using (var game = new Map2dGame())
+				game.Run();
+		}
+	}
+}
---
+++ Map2D/src/core/Map2dGame.cs
@@ -0,0 +1,74 @@
+using Map2D.assets;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Map2D.core
+{
+    public class Map2dGame : Game
+    {
+        private readonly GraphicsDeviceManager graphics;
+        private SpriteBatch spriteBatch;
+
+        public Map2dGame()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "res";
+        }
+
+        protected override void Initialize()
+        {
+            Point monitorSize = new Point(
+                GraphicsAdapter.DefaultAdapter.CurrentDisplayMode.Width, 
+                GraphicsAdapter.DefaultAdapter.CurrentDisplayMode.Height);
+
+            Point windowSize = new Point(1200, 800);
+            
+            IsFixedTimeStep = false;
+            graphics.SynchronizeWithVerticalRetrace = false;
+            
+            if (monitorSize == windowSize)
+               graphics.IsFullScreen = true;
+            
+            IsMouseVisible = true;
+            Window.AllowUserResizing = false;
+            Window.IsBorderless = false;
+            Window.Title = "Map2D";
+            Window.Position = new Point(
+                (int) (monitorSize.X / 2f - windowSize.X / 2f), 
+                (int) (monitorSize.Y / 2f - windowSize.Y / 2f));
+
+            graphics.PreferredBackBufferWidth = windowSize.X;
+            graphics.PreferredBackBufferHeight = windowSize.Y;
+            graphics.ApplyChanges();
+
+            base.Initialize();
+        }
+        
+        protected override void LoadContent()
+        {
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+            
+            Assets.Initialize(Content);
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            // Handle exit press
+            
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.Black);
+            
+            spriteBatch.Begin();
+            
+            
+
+            spriteBatch.End();
+            
+            base.Draw(gameTime);
+        }
+    }
+}
