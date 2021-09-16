b6e9a16803fb083701fcbe1a0bd34f9baecc44b6
Leon Steinbach <leon@steinba.de>
2021-09-16 23:15:56 +0200

    Added radio button and group as gui element

---
+++ Map2D/src/graphics/gui/RadioButton.cs
@@ -0,0 +1,49 @@
+﻿using System.Runtime.CompilerServices;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Map2D.graphics.gui
+{
+	public class RadioButton : Checkbox
+	{
+		private RadioGroup radioGroup;
+
+		public RadioButton(RadioGroup radioGroup, bool selected, Texture2D texture, 
+			Texture2D checkedTexture, Vector2 position, int margin, string text, 
+			SpriteFont font, Color color, bool centered = true, float layer = 1) :
+			this(radioGroup, selected, texture, texture, checkedTexture, position,
+				margin, null, null, text, font, color, 0, 1f, 0f, centered, layer)
+		{
+
+		}
+
+		public RadioButton(RadioGroup radioGroup, bool selected, Texture2D defaultTexture, 
+			Texture2D hoverTexture, Texture2D checkedTexture, Vector2 position, int margin, 
+			string text, SpriteFont font, Color color, bool centered = true, float layer = 1) :
+			this(radioGroup, selected, defaultTexture, hoverTexture, checkedTexture, position,
+				margin, null, null, text, font, color, 0, 1f, 0f, centered, layer)
+		{
+
+		}
+
+		public RadioButton(RadioGroup radioGroup, bool selected, Texture2D defaultTexture, 
+			Texture2D hoverTexture, Texture2D checkedTexture, Vector2 position, int margin, 
+			string hoverSound, string clickSound, string text, SpriteFont font, Color color, 
+			int hoverOffset, float hoverScale, float hoverAngle, bool centered = true, float layer = 1) :
+			base(selected, defaultTexture, hoverTexture, checkedTexture, position,
+				margin, hoverSound, clickSound, text, font, color,
+				hoverOffset, hoverScale, hoverAngle, centered, layer)
+		{
+			this.radioGroup = radioGroup;
+		}
+
+		public new void Update()
+		{
+			if (Pressed() && !Checked) {
+				foreach (RadioButton other in radioGroup.Buttons)
+					other.Checked = false;
+				Checked = true;
+			}
+		}
+	}
+}
---
+++ Map2D/src/graphics/gui/RadioGroup.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections.Generic;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Map2D.graphics.gui
+{
+	public class RadioGroup
+	{
+		public RadioGroup()
+		{
+			Buttons = new List<RadioButton>();
+		}
+
+		public RadioGroup(List<RadioButton> buttons)
+		{
+			Buttons = buttons;
+		}
+
+		public List<RadioButton> Buttons { get; set; }
+
+		public RadioButton Checked
+		{
+			get {
+				foreach (RadioButton button in Buttons) {
+					if (button.Checked)
+						return button;
+				}
+
+				return null;
+			}
+		}
+
+		public int CheckedIndex
+		{
+			get {
+				for (int i = 0; i < Buttons.Count; i++) {
+					if (Buttons[i].Checked)
+						return i;
+				}
+
+				return -1;
+			}
+			set {
+				foreach (RadioButton button in Buttons)
+					button.Checked = false;
+				Buttons[value].Checked = true;
+			}
+		}
+
+		public void Update()
+		{
+			foreach (RadioButton button in Buttons)
+				button.Update();
+		}
+
+		public void UpdateSounds()
+		{
+			foreach (RadioButton button in Buttons)
+				button.UpdateSounds();
+		}
+
+		public void Render(SpriteBatch spriteBatch)
+		{
+			foreach (RadioButton button in Buttons)
+				button.Render(spriteBatch);
+		}
+	}
+}
