41ea2c868d91f8e60f0ff5f6cd9447a150c35208
Leon Steinbach <leon@steinba.de>
2021-09-20 22:24:26 +0200

    Added basic abstract body class

---
+++ Map2D/src/physics/Body.cs
@@ -0,0 +1,18 @@
+﻿using Map2D.math.geometry;
+using Microsoft.Xna.Framework;
+
+namespace Map2D.physics
+{
+	public abstract class Body
+	{
+		public Vector2 Position { get; set; }
+		public Vector2 Velocity { get; set; }
+		public Vector2 Acceleration { get; set; }
+		public Vector2 Rotation { get; set; }
+		public GeometricObject BoundingBox { get; set; }
+
+		// TODO: Update, RenderBoundingBox (debug), forces, center of mass (in math)
+		// TODO: Add body types (static, dynamic, rotatable)
+		// TODO: Add world structure (list of bodies)
+	}
+}
