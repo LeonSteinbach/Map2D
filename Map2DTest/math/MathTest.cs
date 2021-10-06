a667b98478bcc5e185b27a0b9ea602df53ac2586
LeonSteinbach <leon@steinba.de>
2021-10-06 13:18:53 +0200

    Added tests (for testing the fine code coverage extension)

--- Map2DTest/math/MathTest.cs
+++ Map2DTest/math/MathTest.cs
@@ -16,6 +16,18 @@ namespace Map2DTest
             Vector2 b = new Vector2(1, 1);
 
             Assert.AreEqual(Math.Sqrt(2), MathHelper.Distance(a, b), 0.0001d);
+            Assert.AreEqual(0.785398, MathHelper.AngleRadians(a, b), 0.0001d);
+
+            Assert.AreEqual(0, MathHelper.RadiansToDegrees(0));
+            Assert.AreEqual(90, MathHelper.RadiansToDegrees((float)Math.PI / 2), 0.0001d);
+            Assert.AreEqual(180, MathHelper.RadiansToDegrees((float) Math.PI), 0.0001d);
+            Assert.AreEqual(360, MathHelper.RadiansToDegrees((float)Math.PI * 2), 0.0001d);
+
+            Assert.AreEqual(0, MathHelper.DegreesToRadians(0));
+
+            Assert.AreEqual(0, MathHelper.MapRange(0f, 0f, 1f, 0f, 2f));
+            Assert.AreEqual(2, MathHelper.MapRange(1f, 0f, 1f, 0f, 2f));
+            Assert.AreEqual(-2, MathHelper.MapRange(-1f, 0f, 1f, 0f, 2f));
         }
     }
 }
