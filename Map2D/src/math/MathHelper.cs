bc037311ddb26b815e4a417628002b875e6abca6
LeonSteinbach <leon@steinba.de>
2021-10-06 13:18:21 +0200

    Changed method name from constrain to mapRange

--- Map2D/src/math/MathHelper.cs
+++ Map2D/src/math/MathHelper.cs
@@ -40,7 +40,7 @@ namespace Map2D.math
             return angle * (float) Math.PI / 180f;
         }
         
-        public static float Constrain(float x, float inMin, float inMax, float outMin, float outMax)
+        public static float MapRange(float x, float inMin, float inMax, float outMin, float outMax)
         {
             return (x - inMin) * (outMax - outMin) / (inMax - inMin) + outMin;
         }
